5a1abafa73d0b92cb2970e0005bfd945dcd5c093
twentyone37 <twentyone_37@proton.me>
2023-06-22 11:38:06 +0200

    Add aragon osx

--- test/CryptoMappDao.test.ts
+++ test/CryptoMappDao.test.ts
@@ -19,4 +19,11 @@ describe("CryptoMappDao", () => {
     await cryptoMappDao.setRegistrationFee(1000);
     expect(await cryptoMappDao.registrationFee()).to.equal(1000);
   });
+
+  it("should allow members to create proposals", async () => {
+    await cryptoMappDao.createProposal("Change registration fee", 10);
+    const proposal = await cryptoMappDao.proposals(0);
+    expect(proposal.description).to.equal("Change registration fee");
+    expect(proposal.endBlock).to.equal(10);
+  });
 });
