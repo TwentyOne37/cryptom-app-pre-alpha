3c209ed2a3d9f4e839aa0fa825e4a173d50e6676
twentyone37 <twentyone_37@proton.me>
2023-07-17 17:44:06 +0200

    Add ERC20s contracts and their deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    arbitrum_goerli: {
+      url: process.env.ARBITRUM_GOERLI_INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
+  etherscan: {
+    apiKey: "${process.env.ETHERSCAN_API_KEY}",
+  },
 };
 
 export default config;
---
+++ scripts/deployERC20.js
@@ -0,0 +1,55 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  // Deploy DAI contract
+  const DAI = await hre.ethers.getContractFactory("DAI");
+  const dai = await DAI.deploy(ethers.parseUnits("1000", "ether"));
+  await dai.waitForDeployment();
+  console.log("DAI deployed to:", await dai.getAddress());
+
+  // Deploy USDC contract
+  const USDC = await hre.ethers.getContractFactory("USDC");
+  const usdc = await USDC.deploy(ethers.parseUnits("1000", "ether"));
+  await usdc.waitForDeployment();
+  console.log("USDC deployed to:", await usdc.getAddress());
+
+  // Deploy USDT contract
+  const USDT = await hre.ethers.getContractFactory("USDT");
+  const usdt = await USDT.deploy(ethers.parseUnits("1000", "ether"));
+  await usdt.waitForDeployment();
+  console.log("USDT deployed to:", await usdt.getAddress());
+
+  // DAI
+  const daiTransferTx = await dai.transfer(
+    deployer.address,
+    ethers.parseUnits("1000", "ether")
+  );
+  await daiTransferTx.wait();
+  console.log("DAI tokens have been sent to:", deployer.address);
+
+  // USDC
+  const usdcTransferTx = await usdc.transfer(
+    deployer.address,
+    ethers.parseUnits("1000", "ether")
+  );
+  await usdcTransferTx.wait();
+  console.log("USDC tokens have been sent to:", deployer.address);
+
+  // USDT
+  const usdtTransferTx = await usdt.transfer(
+    deployer.address,
+    ethers.parseUnits("1000", "ether")
+  );
+  await usdtTransferTx.wait();
+  console.log("USDT tokens have been sent to:", deployer.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
